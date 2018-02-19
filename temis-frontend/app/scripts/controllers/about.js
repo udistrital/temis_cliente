d710f8bdab2ba571a0ccab29f1221447982e77bc
andreyCristian <onep.300@gmail.com>
2018-02-18 20:32:29 -0500

    start build

---
+++ temis-frontend/app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name temisApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the temisApp
+ */
+angular.module('temisApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ temis-frontend/app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name temisApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the temisApp
+ */
+angular.module('temisApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
